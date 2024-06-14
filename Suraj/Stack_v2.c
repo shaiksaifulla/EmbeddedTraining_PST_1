d8d8f21ecc8c03d6e1badc5d6214123a1e845264
suraj <surajkotha2015@gmail.com>
2024-06-14 12:27:50 -0400

    Added stack programs

---
+++ Suraj/Stack_v2.c
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdio.h>
+#define SIZE 10
+#define NUMBER_OF_ELEMENTS 5
+#define STACK_FULL -1
+#define STACK_EMPTY -2
+
+
+int push(int arr[SIZE], int arr2[NUMBER_OF_ELEMENTS]);
+int pop(int arr[SIZE]);
