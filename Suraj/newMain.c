7f06fc4c2820a4f7a443958676c01822213442e1
suraj <surajkotha2015@gmail.com>
2024-06-16 09:01:44 -0400

    Added single linked list program

---
+++ Suraj/newMain.c
@@ -0,0 +1,22 @@
+#include "sLinkedlist.h"
+
+int main()
+{
+	struct node nodeElement;
+	nodeElement.data = 10;
+	nodeElement.next = NULL;
+
+	addNode(&nodeElement);
+
+	nodeElement.data = 20;
+	nodeElement.next = NULL;
+
+	addNode(&nodeElement);
+
+	nodeElement.data = 30;
+	nodeElement.next = NULL;
+
+	addNode(&nodeElement);
+
+	printList();
+}
