7eefd1ca9b5167414fbfe88416b942e442b4eee2
Alekhya <alekhya.nettam@gmail.com>
2024-06-24 19:21:31 -0400

    Adding files

---
+++ Alekhya/ArraysPolynomial.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define a 10
+#define b 20
+#define c 30
+#define SIZE 10
+/*
+Write a function to declare and initialize an array of type double, size = 10 elements.
+Initialize the array element with polynomial ax^2 + bx + c.
+where x represent the array index for an element
+a,b,c are 10,20,30 respectively.
+use macros for a,b,c and SIZE.
+print the array values in the following fashion
+
+[	123.45		456.23		678.23		4578.23		1234.45	]
+*/
+
+
+void polynomial() {
+    double array[SIZE] = {};
+    for (int index = 0; index < SIZE; index++) {
+        array[index] = (a) * (index * index) + (b * index) + c;
+        printf("%.2f\t", array[index]);
+    }
+}
+
+int main()
+{
+    polynomial();
+}
---
+++ Alekhya/Stack_V2.h
@@ -0,0 +1,9 @@
+#pragma once
+#define SIZE 20
+#define STACK_FULL -1
+#define NUM_OF_ELEMENTS 5
+#define STACK_EMPTY -2
+#define SUCCESS 0
+
+int push(int*, int*);
+int pop(int*);
